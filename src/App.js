393d98a3a5205f860ea7b27fbb6d5e1559227df6
AkintolaDavid <akintoladavid66@gmail.com>
2023-10-05 13:33:49 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import PersonIcon from '@mui/icons-material/Person';
+function App() {
+  // return <Profile/>
+  return <Home/>
+}
+
+export default App;
